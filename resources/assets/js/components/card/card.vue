0b31135b1823009330dc6f4dfec3df95e0c7c083
valdislav i <vladoss_19995@mail.ru>
2019-12-18 18:00:25 +0300

    some fix

--- resources/assets/js/components/card/card.vue
+++ resources/assets/js/components/card/card.vue
@@ -1,19 +1,20 @@
 <template lang="pug">
     .card(:style="{backgroundImage: 'url('+'./images/bg.png'+')'}")
       .card__content
-        .card__title {{ items.title}}
-        .card__footer
-          a(href="#").card__link.card__category
-            .card__icon
-              include folder.svg
-            .card__text {{items.category}}
-          .card__details
-            a(href="#").card__link
-              include user.svg
-            a(href="#").card__link
-              include calendar.svg
-            a(href="#").card__link
-              include comment.svg
+        .card__wrapper
+          .card__title {{ items.title}}
+          .card__footer
+            a(href="#").card__link.card__category
+              .card__icon
+                include folder.svg
+              .card__text {{items.category}}
+            .card__details
+              a(href="#").card__link
+                include user.svg
+              a(href="#").card__link
+                include calendar.svg
+              a(href="#").card__link
+                include comment.svg
 
 </template>
 
